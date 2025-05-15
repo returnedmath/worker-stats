a2ea52168bcf4130fcb6de0522d054153b598480
eSourceStorage <minecraftfiftypantheon@hotmail.com>
2025-05-15 15:41:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ export default {
 
     const headers = {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "http://returnedmath.xyz",
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type",
     };
